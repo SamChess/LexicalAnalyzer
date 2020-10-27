9a9eae922b806b86b4712143156ca50ad013115f
unknown <fcbsammymulwa@gmail.com>
2020-10-27 04:40:02 +0300

    lexical analzer C++

---
+++ main.hpp
@@ -0,0 +1,10 @@
+/*main header file*/
+/*the header file for main.cpp*/
+
+#include <iostream>
+#include<math.h>
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+using namespace std;
